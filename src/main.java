d7678781f7fc3a9aa3d487464ae819bd16899906
Marina <marina.butterfly95@gmail.com>
2022-03-07 18:37:38 +0200

    Al doilea commnit

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.print("Tananana");
-        System.out.println("Blalalla");
+        System.out.println("UUUUU");
+        System.out.println("AAAAAA");
     }
 }
